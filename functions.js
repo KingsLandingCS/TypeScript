43e1a6a52babad76bc1a9168fc950e60095d735b
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-21 19:39:50 +0500

    learning

--- functions.js
+++ functions.js
@@ -1,34 +1,18 @@
-function add(num1: number, num2: number, num3?: number): number {
-
+"use strict";
+function add(num1, num2, num3) {
     return num3 ? num1 + num2 + num3 : num1 + num2;
 }
-
 console.log(add(10, 20));
-
 console.log(add(10, 20, 25));
-
-
-
-const sub = (num1: number, num2: number, num3 = 10): number => num1 - num2 - num3;
-
+const sub = (num1, num2, num3 = 10) => num1 - num2 - num3;
 console.log(sub(10, 20));
-
 console.log(sub(10, 20, 5));
-
-
-const mult = function (num1: number, num2: number): number {
-
+const mult = function (num1, num2) {
     return num1 * num2;
-}
-
+};
 console.log(mult(10, 20));
-
-
-function add2(num1: number, num2: number, ...num3: number[]): number {
-
+function add2(num1, num2, ...num3) {
     return num1 + num2 + num3.reduce((a, b) => a + b);
 }
-
 let numbers = [1, 2, 3, 4, 5];
 console.log(add2(2, 3, ...numbers));
-
